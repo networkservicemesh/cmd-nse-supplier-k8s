7a29a96b83deaaa3e2291c17265061d745885c06
Danil Uzlov <DanilUzlov@yandex.ru>
2021-05-18 15:54:49 +0700

    add main.go code
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Doc.ai and/or its affiliates.
+// Copyright (c) 2020-2021 Doc.ai and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -14,7 +14,268 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !windows
+
 package main
 
+import (
+	"context"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/edwarnicke/grpcfd"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	registryapi "github.com/networkservicemesh/api/pkg/api/registry"
+
+	"github.com/networkservicemesh/sdk-k8s/pkg/networkservice/common/createpod"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/endpoint"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
+	registryclient "github.com/networkservicemesh/sdk/pkg/registry/chains/client"
+	"github.com/networkservicemesh/sdk/pkg/tools/debug"
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+	"github.com/networkservicemesh/sdk/pkg/tools/jaeger"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
+	"github.com/networkservicemesh/sdk/pkg/tools/opentracing"
+	"github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
+)
+
+// Config holds configuration parameters from environment variables
+type Config struct {
+	Name             string            `default:"nse-supplier-k8s" desc:"Name of the Server"`
+	ConnectTo        url.URL           `default:"unix:///var/lib/networkservicemesh/nsm.io.sock" desc:"url to connect to" split_words:"true"`
+	MaxTokenLifetime time.Duration     `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	ServiceName      string            `default:"nse-supplier-k8s" desc:"Name of providing service" split_words:"true"`
+	Payload          string            `default:"ETHERNET" desc:"Name of provided service payload" split_words:"true"`
+	Labels           map[string]string `default:"" desc:"Endpoint labels"`
+	PodDescription   string            `default:"pod.yaml" desc:"Path to the file that describes pod to be created"`
+}
+
+// Process prints and processes env to config
+func (c *Config) Process() error {
+	if err := envconfig.Usage("nse", c); err != nil {
+		return errors.Wrap(err, "cannot show usage of envconfig nse")
+	}
+	if err := envconfig.Process("nse", c); err != nil {
+		return errors.Wrap(err, "cannot process envconfig nse")
+	}
+	return nil
+}
+
 func main() {
+	// ********************************************************************************
+	// setup context to catch signals
+	// ********************************************************************************
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		// More Linux signals here
+		syscall.SIGHUP,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+	defer cancel()
+
+	// ********************************************************************************
+	// setup logging
+	// ********************************************************************************
+	logrus.SetFormatter(&nested.Formatter{})
+	ctx = log.WithFields(ctx, map[string]interface{}{"cmd": os.Args[0]})
+	ctx = log.WithLog(ctx, logruslogger.New(ctx))
+
+	if err := debug.Self(); err != nil {
+		log.FromContext(ctx).Infof("%s", err)
+	}
+
+	// ********************************************************************************
+	// Configure open tracing
+	// ********************************************************************************
+	log.EnableTracing(true)
+	jaegerCloser := jaeger.InitJaeger(ctx, "cmd-nse-icmp-responder")
+	defer func() { _ = jaegerCloser.Close() }()
+
+	// enumerating phases
+	log.FromContext(ctx).Infof("there are 6 phases which will be executed followed by a success message:")
+	log.FromContext(ctx).Infof("the phases include:")
+	log.FromContext(ctx).Infof("1: get config from environment")
+	log.FromContext(ctx).Infof("2: retrieve spiffe svid")
+	log.FromContext(ctx).Infof("3: create icmp server ipam")
+	log.FromContext(ctx).Infof("4: create icmp server nse")
+	log.FromContext(ctx).Infof("5: create grpc and mount nse")
+	log.FromContext(ctx).Infof("6: register nse with nsm")
+	log.FromContext(ctx).Infof("a final success message with start time duration")
+
+	starttime := time.Now()
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 1: get config from environment")
+	// ********************************************************************************
+	config := new(Config)
+	if err := config.Process(); err != nil {
+		logrus.Fatal(err.Error())
+	}
+
+	log.FromContext(ctx).Infof("Config: %#v", config)
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 2: retrieving svid, check spire agent logs if this is the last line you see")
+	// ********************************************************************************
+	source, err := workloadapi.NewX509Source(ctx)
+	if err != nil {
+		logrus.Fatalf("error getting x509 source: %+v", err)
+	}
+	svid, err := source.GetX509SVID()
+	if err != nil {
+		logrus.Fatalf("error getting x509 svid: %+v", err)
+	}
+	log.FromContext(ctx).Infof("SVID: %q", svid.ID)
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 3: getting kubernetes config")
+	// ********************************************************************************
+	// build configuration from the config file.
+	kubeConfig, err := rest.InClusterConfig()
+	if err != nil {
+		log.FromContext(ctx).Fatalf("can't get kuberneted config. Are you running this app inside kuberneted pod?")
+	}
+	client, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		log.FromContext(ctx).Fatalf("can't get kubernetes client")
+	}
+	log.FromContext(ctx).Infof("successfully obtained kubernetes client")
+
+	podYamlFile, err := ioutil.ReadFile(config.PodDescription)
+	if err != nil {
+		log.FromContext(ctx).Fatalf("can't read pod file: %+v", err)
+	}
+	var podDesc corev1.Pod
+	err = yaml.Unmarshal(podYamlFile, &podDesc)
+	if err != nil {
+		log.FromContext(ctx).Fatalf("can't unmarshal pod file: %+v", err)
+	}
+	log.FromContext(ctx).Infof("successfully parsed pod description")
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 4: create network service endpoint")
+	// ********************************************************************************
+	responderEndpoint := endpoint.NewServer(ctx,
+		spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime),
+		endpoint.WithName(config.Name),
+		endpoint.WithAuthorizeServer(authorize.NewServer()),
+		endpoint.WithAdditionalFunctionality(
+			createpod.NewServer(client, &podDesc),
+		),
+	)
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 5: create grpc server and register the server")
+	// ********************************************************************************
+	options := append(
+		opentracing.WithTracing(),
+		grpc.Creds(
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny()),
+				),
+			),
+		),
+	)
+	server := grpc.NewServer(options...)
+	responderEndpoint.Register(server)
+	tmpDir, err := ioutil.TempDir("", config.Name)
+	if err != nil {
+		logrus.Fatalf("error creating tmpDir %+v", err)
+	}
+	defer func(tmpDir string) { _ = os.Remove(tmpDir) }(tmpDir)
+	listenOn := &(url.URL{Scheme: "unix", Path: filepath.Join(tmpDir, "listen.on")})
+	srvErrCh := grpcutils.ListenAndServe(ctx, listenOn, server)
+	exitOnErr(ctx, cancel, srvErrCh)
+	log.FromContext(ctx).Infof("grpc server started")
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 6: register nse with nsm")
+	// ********************************************************************************
+	clientOptions := append(
+		opentracing.WithTracingDial(),
+		grpc.WithBlock(),
+		grpc.WithDefaultCallOptions(grpc.WaitForReady(true)),
+		grpc.WithTransportCredentials(
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny()),
+				),
+			),
+		),
+	)
+	cc, err := grpc.DialContext(ctx,
+		grpcutils.URLToTarget(&config.ConnectTo),
+		clientOptions...,
+	)
+	if err != nil {
+		log.FromContext(ctx).Fatalf("error establishing grpc connection to registry server %+v", err)
+	}
+
+	_, err = registryclient.NewNetworkServiceRegistryClient(cc).Register(context.Background(), &registryapi.NetworkService{
+		Name:    config.ServiceName,
+		Payload: config.Payload,
+	})
+
+	if err != nil {
+		log.FromContext(ctx).Fatalf("unable to register ns %+v", err)
+	}
+
+	registryClient := registryclient.NewNetworkServiceEndpointRegistryClient(ctx, cc)
+	nse, err := registryClient.Register(context.Background(), &registryapi.NetworkServiceEndpoint{
+		Name:                config.Name,
+		NetworkServiceNames: []string{config.ServiceName},
+		NetworkServiceLabels: map[string]*registryapi.NetworkServiceLabels{
+			config.ServiceName: {
+				Labels: config.Labels,
+			},
+		},
+		Url: listenOn.String(),
+	})
+	logrus.Infof("nse: %+v", nse)
+
+	if err != nil {
+		log.FromContext(ctx).Fatalf("unable to register nse %+v", err)
+	}
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("startup completed in %v", time.Since(starttime))
+	// ********************************************************************************
+
+	// wait for server to exit
+	<-ctx.Done()
+}
+
+func exitOnErr(ctx context.Context, cancel context.CancelFunc, errCh <-chan error) {
+	// If we already have an error, log it and exit
+	select {
+	case err := <-errCh:
+		log.FromContext(ctx).Fatal(err)
+	default:
+	}
+	// Otherwise wait for an error in the background to log and cancel
+	go func(ctx context.Context, errCh <-chan error) {
+		err := <-errCh
+		log.FromContext(ctx).Error(err)
+		cancel()
+	}(ctx, errCh)
 }
